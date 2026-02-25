ed94a5356629cb13ac3d6afd3391f0dece1d36c6
intern1-ctrl <intern1@markwave.ai>
2026-02-25 17:56:55 +0530

    bugs fixed

--- src/pages/Dashboard/ApprovalsPage.tsx
+++ src/pages/Dashboard/ApprovalsPage.tsx
@@ -94,8 +94,8 @@ export default function ApprovalsPage() {
                                 : "text-gray-500 hover:text-gray-700 dark:text-gray-300 dark:hover:text-white"
                                 }`}
                         >
-                            <LockIcon className="size-4.5" />
-                            <span>Admin Final</span>
+                            <UserCircleIcon className="size-4.5" />
+                            <span>Admin Review</span>
                         </button>
                     </div>
                 </div>
